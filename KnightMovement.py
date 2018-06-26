3a6abc148f11f0d80fd6dfff5343aac77a7faf7c
Dan Magaril <coolfencer@gmail.com>
2018-06-26 13:18:12 -0700

    Edited out merge conflicts

--- KnightMovement.py
+++ KnightMovement.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import turtle
 #5x5 (boardSize x boardSize)
 boardSize=5
@@ -52,13 +51,6 @@ def clearStamp(c,r):
 #    turtle.shape(blank)
     turtle.stamp()
 
-    
-=======
-#6x6 (boardSize x boardSize)
-boardSize=6
-startX=1;startY=6
-endX=4;endY=2
->>>>>>> 6x6_Board
 def setDirections(arr):
     arr=[0,0,0,0,0,0,0,0] #sets all direction movement to false (will be changed to true if knight can move in any direction)
     return arr
@@ -224,8 +216,6 @@ step=0
 stampAt(x,y)
 while True:
     if finalLocation(x,y,endX,endY,xPos):
-<<<<<<< HEAD
-=======
         break
     if noSolution(x,y,directions):
         print("NO SOLUTION")
@@ -233,7 +223,6 @@ while True:
     if x==startX and y==startY and sum(directions)<2 and len(xPos)<2:
         print("--------------------------RETURNED TO ORIGINAL PLACE-------------------------------")
         print(directions)
->>>>>>> 6x6_Board
         break
     if not checkForRepeatLocation(x,y,xPos,yPos) and not checkIfAllDirectionsFalse(directions) and not falseFinal(x,y,endX,endY,xPos):
         possMoves.append(directions)
