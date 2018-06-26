f566ede8dcbec2de3e416d135778cfe1031c7010
Dan Magaril <coolfencer@gmail.com>
2018-06-26 15:43:42 -0700

    Board size is now consistent, square size is based on number needed

--- turtle init test.py
+++ turtle init test.py
@@ -1,6 +1,6 @@
 import turtle
 #5x5 (boardSize x boardSize)
-boardSize=5
+boardSize=8
 startX=1;startY=5
 endX=3;endY=3
 pic="unicorn.gif"
@@ -8,24 +8,29 @@ blank ="square.gif"
 #turtle formula: goto(col*100-350,row*100+350)
 
 def initTurtle():
-    turtle.setup(1000,1000)
+    global sqSize
+    turtle.setup(1000,1000,0,0)
+    winH=turtle.window_height()-20;winW=turtle.window_width()-20
+    turtle.title("Knight's Tour")
+    turtle.setworldcoordinates(0,1000,1000,0)
     turtle.ht()
     turtle.pen(pencolor="black")
     turtle.pen(pensize=3)
-    turtle.speed(500)
-    goto(-400,400)
+    turtle.speed(0)
+    if(winW<winH):
+        sqSize=winW/(boardSize+1)
+    else:
+        sqSize=winH/(boardSize+1)
     for r in range(0,boardSize+1): #draw horizontal rows
-        goto(-400,-r*100+400)
-        
-        drto(boardSize*100-400,-r*100+400)
-    goto(-400,400)
+        goto(30,r*sqSize+30)
+        drto(boardSize*sqSize+30,r*sqSize+30)
+    
     for c in range(0,boardSize+1): #draw vertical columns
-        goto((c)*100-400,400)
-        drto(c*100-400,-boardSize*100+400)
+        goto((c)*sqSize+30,30)
+        drto(c*sqSize+30,boardSize*sqSize+30)
     turtle.Screen().addshape("unicorn.gif")
     turtle.Screen().addshape("square.gif")
-    turtle.shape(pic)    
-    
+    turtle.shape(pic)
     
 def goto(x,y):
     turtle.pu()
@@ -36,15 +41,17 @@ def drto(x,y):
     turtle.goto(x,y)
 
 def stampAt(c,r):
-    goto((c-1)*100-350,350-(boardSize-r)*100)
+    goto((c-1)*sqSize+80,(boardSize-r+1)*sqSize-30)
     turtle.shape(pic)
     turtle.stamp()
 
 def clearStamp(c,r):
-    goto((c-1)*100-350,350-(boardSize-r)*100)
+    goto((c-1)*sqSize+80,(boardSize-r+1)*sqSize-30)
     turtle.shape(blank)
     turtle.stamp()
-
+    
+def clr():
+    turtle.undo()
 
 
 
@@ -56,8 +63,8 @@ stampAt(5,1)
 stampAt(8,8)
 stampAt(8,2)
 clearStamp(1,1)
-
-
+while True:
+    clr()
 
 
 
